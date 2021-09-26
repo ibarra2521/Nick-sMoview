4e60dc7a9ed0627b22cedfd2aceac6c4562a1a01
Nick Ibarra <ibarra2521@gmail.com>
2021-09-26 17:46:52 -0500

    Initial Commit

---
+++ Shared/Nick_sMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nick_sMovieApp.swift
+//  Shared
+//
+//  Created by Nivardo Ibarra on 9/26/21.
+//
+
+import SwiftUI
+
+@main
+struct Nick_sMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
